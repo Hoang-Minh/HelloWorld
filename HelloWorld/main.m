e4f6d844a9d94360278e3254b3f8ac7c9a25021b
hoangminh <hoangminh@hoangminhs-Mac.local>
2014-09-06 01:49:46 -0700

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by hoangminh on 9/6/14.
+//  Copyright (c) 2014 Minh Vu Hoang Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
